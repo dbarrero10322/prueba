035a28048845a9face02014485c7caaab0b12457
Daniel Barrero Reyes <dbarrero8805@gmail.com>
2020-02-20 10:00:51 -0500

    manifest

---
+++ trabajo.py
@@ -0,0 +1,18 @@
+{
+    'name': '',
+    'version': '',
+    'summary': '',
+    'description': '',
+    'category': '',
+    'author': '',
+    'website': '',
+    'license': '',
+    'depends': [''],
+    'data': [''],
+    'demo': [''],
+    'installable': True,
+    'auto_install': False,
+    'external_dependencies': {
+        'python': [''],
+    }
+}
